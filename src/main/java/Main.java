66cca0053e2408db870acc671b471801a05f2849
Andre Matutat <amatutat@web.de>
2021-11-23 18:18:06 +0100

    cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,4 @@
-import graphg.CantBePlanarException;
-import graphg.Graph;
-import graphg.GraphG;
-import graphg.NoSolutionException;
-
-import java.io.FileNotFoundException;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws CantBePlanarException, NoSolutionException {
-        GraphG g = new GraphG();
-        //g.writeToJSON(g.generateGraphs(5, 2),"5_2.json");
-
-        List<Graph> so = null;
-        try {
-            so = g.readFromJson("5_2.json");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-        so.forEach(s -> System.out.println(s.toDot()));
+    public static void main(String[] args) {
     }
 }
