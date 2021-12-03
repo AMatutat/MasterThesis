15798e9e9ab19a06f6b8b934be8f97f45b02f22d
Andre Matutat <amatutat@web.de>
2021-12-03 14:56:09 +0100

    some skeleton

---
+++ src/main/java/levelg/LevelG.java
@@ -0,0 +1,15 @@
+package levelg;
+
+import stuff.DesignLabel;
+
+public class LevelG {
+
+    // todo
+    public Level getLevel(int nodecounter, int edgecounter, DesignLabel label) {
+        // getGraph
+        // getRoomTemplates
+        // make level
+        // replace placeholder in rooms
+        return null;
+    }
+}
--- src/main/java/levelg/Room.java
+++ src/main/java/levelg/Room.java
@@ -5,9 +5,14 @@ import stuff.DesignLabel;
 public class Room {
     private int[][] layout;
     private DesignLabel design;
+    private int index;
 
     public Room(int[][] layout, DesignLabel label) {
         this.layout = layout;
         this.design = label;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 }
